d9b09f9c6e4fdfff0a79f8abb39ac8564b0908cb
shadeed-winner <87156808+shadeed-winner@users.noreply.github.com>
2021-12-03 17:49:16 -0600

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGxjATQB0iAysICdVGNonBv1fxJQOmtN4",
+    authDomain: "letschat-e3070.firebaseapp.com",
+    projectId: "letschat-e3070",
+    storageBucket: "letschat-e3070.appspot.com",
+    messagingSenderId: "372924479337",
+    appId: "1:372924479337:web:e4b68ca96fc6a1a69beff8",
+    measurementId: "G-YRMG5QBDGF"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
